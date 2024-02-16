9dcac1ca8b4b372f1aae9a9ec622b3d9cca39b1e
Junior <eecfredes@gmail.com>
2024-02-15 21:19:51 -0300

    :rocket: User dto, criado

---
+++ servers/apps/users/src/dto/user.dto.ts
@@ -0,0 +1,32 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsString, IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+@InputType()
+export class RegisterDto {
+  @Field()
+  @IsNotEmpty({ message: 'Name is required.' })
+  @IsString({ message: 'Name must need to be one string.' })
+  name: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is required.' })
+  @MinLength(8, { message: 'Password must be at least 8 characters.' })
+  password: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is not valid.' })
+  email: string;
+}
+
+@InputType()
+export class LoginDto {
+  @Field()
+  @IsNotEmpty({ message: 'Email is required.' })
+  @IsEmail({}, { message: 'Email is not valid.' })
+  email: string;
+
+  @Field()
+  @IsNotEmpty({ message: 'Password is required.' })
+  password: string;
+}
