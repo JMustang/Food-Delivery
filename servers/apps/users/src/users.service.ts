368db619e869aed3293cae8b0237812bf1ec473a
Junior <eecfredes@gmail.com>
2024-02-18 00:37:38 -0300

    :rocket: Configurando o user service

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,12 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(
+    private readonly JwtService: JwtService,
+    // private readonly prisma
+    private readonly ConfigService: ConfigService,
+  ) {}
 }
