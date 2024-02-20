a47d18aaafe5e2101c9f427152fbd1f4238c9683
Junior <eecfredes@gmail.com>
2024-02-19 22:27:45 -0300

    :rocket: login metodo criado

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
-import { RegisterDto } from './dto/user.dto';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
@@ -12,7 +12,6 @@ export class UsersService {
   ) {}
 
   // register user
-
   async register(registerDto: RegisterDto) {
     const { name, email, password } = registerDto;
     const user = {
@@ -20,6 +19,13 @@ export class UsersService {
       email,
       password,
     };
+    return user;
+  }
+
+  // Login service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = { email, password };
     return user;
   }
 }
